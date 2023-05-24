09a583c39ca7c45de888f8a64e953fbc9f64252c
rafa <rafael.ramdeviu.paya@cieep.com>
2023-05-24 09:28:31 +0200

    estilos

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,14 @@ import { View } from "react-native";
 import LoginComponent from "./Login";
 import NavigationComponent from "./Navigation";
 import { UserContext } from "./UserContext";
+import styles from "./styles";
 
 const ContentMain = () => {
   const { user } = useContext(UserContext);
   return (
-    <View>{user === null ? <NavigationComponent /> : <LoginComponent />}</View>
+    <View style={styles.container}>
+      {user === null ? <NavigationComponent /> : <LoginComponent />}
+    </View>
   );
 };
 
