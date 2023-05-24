f44b3b3cc1e3b0d218834113f541821ab4d31776
Victor <victor.bisquertmoscardo@cieep.com>
2023-05-24 15:57:30 +0200

    picker and readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# APP IMPUTACION HORAS
+
+## Tareas Rafa:
+- [ ] Diseño ventana Login
+- [ ] Funcionalidad inputs login y contraseña
+- [ ] Funcionalidad para recordar login
+- [ ] Funcionalidad navegacion entre tabs y ventanas
+## Tareas  Javi:
+
+- [ ] Diseño ventana imputar
+- [ ] Funcionalidad llamadas API
+- [ ] Imputacion proyecto
+- [ ] Buscador de proyectos
+
+
+
+## Tareas Victor:
+- [ ] Diseño de la ventana cerrar ses.
+- [ ] Recoger de la bd la info del user.
+- [ ] Funcionalidad para cerrar sesión.
+- [ ] Select de mes en ventana de horas.
+- [ ] Funcionalidad para recoger las horas seleccionadas.
+- [ ] Funcionalidad botón check ventana horas.
--- src/App/CheckHours/index.js
+++ src/App/CheckHours/index.js
@@ -7,32 +7,21 @@ import { Picker } from "@react-native-picker/picker";
 const CheckHoursComponent = () => {
   const [selectedMonth, setSelectedMonth] = useState(null);
 
-  const handleMonthChange = (month) => {
+  const monthChange = (month) => {
     setSelectedMonth(month);
   };
 
-  const handleButtonPress = () => {
-    console.log("Selected Month:", selectedMonth);
-    // Realiza las acciones que desees con el mes seleccionado
-  };
-
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 0, justifyContent: "center", alignItems: "center" }}>
       <Picker
         selectedValue={selectedMonth}
-        onValueChange={handleMonthChange}
+        onValueChange={monthChange}
         style={{ height: 50, width: 200 }}
       >
-        <Picker.Item label="January" value="January" />
-        <Picker.Item label="February" value="February" />
-        <Picker.Item label="March" value="March" />
-        {/* Agrega más opciones de meses según tus necesidades */}
+        <Picker.Item label="Enero" value="Enero" />
+        <Picker.Item label="Febrero" value="Febrero" />
+        <Picker.Item label="Marzo" value="Marzo" />
       </Picker>
-      <Button
-        title="Check Hours"
-        onPress={handleButtonPress}
-        disabled={!selectedMonth}
-      />
     </View>
   );
 };
