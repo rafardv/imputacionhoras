1ebf86c7e0132aa6b39e2144d467e76d3246c388
rafa <rafael.ramdeviu.paya@cieep.com>
2023-05-24 08:46:37 +0200

    all index

---
+++ src/App/UserContext/index.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState([]);
+
+  u;
+  console.log("useContext", user);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
